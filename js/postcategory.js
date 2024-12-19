a63079e8ca2214d4cdc1b05068afb8bd6877c397
VarchasWingherry <varchas.wingherry@gmail.com>
2024-12-19 19:19:28 +0530

    updated

--- js/postcategory.js
+++ js/postcategory.js
@@ -6,3 +6,27 @@ function checkSelection(event) {
         alert('Please select a category before proceeding.');
     }
 }
+
+
+// scroll for fiexd next btn
+document.addEventListener("DOMContentLoaded", function () {
+    const nextButton = document.querySelector(".next");
+    const category = document.querySelector(".category");
+  
+    function updateButtonPosition() {
+      const categoryRect = category.getBoundingClientRect();
+      const nextButtonHeight = nextButton.offsetHeight;
+  
+      if (categoryRect.bottom <= nextButtonHeight) {
+        // Add class to change the position when .category is scrolled out
+        nextButton.classList.add("scroll-out");
+      } else {
+        // Remove the class when inside .category
+        nextButton.classList.remove("scroll-out");
+      }
+    }
+  
+    // Attach the scroll event listener
+    window.addEventListener("scroll", updateButtonPosition);
+  });
+  
